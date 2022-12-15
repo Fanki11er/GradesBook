2a6223181ee3a2cca6beead5e1f893e3179290e5
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2022-12-15 13:07:31 +0100

    Fixed eror in landing page

--- GradesBook/ClientApp/src/View/LandingPage/LandingPage.tsx
+++ GradesBook/ClientApp/src/View/LandingPage/LandingPage.tsx
@@ -3,16 +3,15 @@ import { routes } from "../../Routes/routes";
 import { LandingPageWrapper } from "./LandingPage.styles";
 
 const LandingPage = () => {
-  const { setting, classes, parentView, program, loginPage, registerPage } =
-    routes;
+  const { setting, classes, parentView, program, login, register } = routes;
   return (
     <LandingPageWrapper>
       <LinkButton to={classes}>Klasy</LinkButton>
       <LinkButton to={setting}>Ustawienia</LinkButton>
       <LinkButton to={parentView}>Rodzice</LinkButton>
       <LinkButton to={program}>Programy</LinkButton>
-      <LinkButton to={loginPage}>Logowanie</LinkButton>
-      <LinkButton to={registerPage}>Rejestracja</LinkButton>
+      <LinkButton to={login}>Logowanie</LinkButton>
+      <LinkButton to={register}>Rejestracja</LinkButton>
     </LandingPageWrapper>
   );
 };
