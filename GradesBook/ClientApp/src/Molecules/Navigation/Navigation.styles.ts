20098eee1ba3fb5de3690920cdc54482c5cd354d
Ewela <ewela28@poczta.onet.pl>
2022-12-15 20:00:59 +0100

    Styled Navigation

--- GradesBook/ClientApp/src/Molecules/Navigation/Navigation.styles.ts
+++ GradesBook/ClientApp/src/Molecules/Navigation/Navigation.styles.ts
@@ -5,20 +5,36 @@ export const NavigationWrapper = styled.nav`
   height: 100%;
   background-color: rgba(217, 217, 217, 1);
   grid-row: 1/2;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  display: grid;
+  grid-template-columns: 100px 1fr 100px;
+  grid-auto-rows: auto;
   align-items: center;
 `;
 
-//export const NavigationImg = styled.img``;
+export const LogoImg = styled.img`
+  width: 55px;
+  height: 55px;
+  margin: 2px 0 0 10px;
+`;
 
-export const NavigationImg = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: gray;
+export const RoleImg = styled.img`
+  width: 35px;
+  height: 35px;
+  margin: 0 10px 0 0;
+`;
+
+export const NameImg = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 0 10px 0 0;
 `;
 
 export const NavigationText = styled.span`
   width: 500px;
+`;
+
+export const RoleAndTextNavigationWrapper = styled.div`
+  width: 400px;
+  display: flex;
+  align-items: center;
 `;
